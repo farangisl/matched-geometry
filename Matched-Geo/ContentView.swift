4ab960f076403606c2ccd7ef150e3907b69b0bba
Sean Allen <allen.sean.t@gmail.com>
2022-07-17 10:24:08 -0400

    Initial Commit

---
+++ Matched-Geo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Matched-Geo
+//
+//  Created by Sean Allen on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Matched-Geo/Matched_GeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matched_GeoApp.swift
+//  Matched-Geo
+//
+//  Created by Sean Allen on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Matched_GeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
